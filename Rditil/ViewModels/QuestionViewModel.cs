e1ca6cd1cf8d524529f8f99cf3622bce921b21b8
FonkyPeaky <Elafi.issam@gmail.com>
2025-05-27 16:18:59 +0200

    test a faire sur QuestionViewModel

--- Rditil/ViewModels/QuestionViewModel.cs
+++ Rditil/ViewModels/QuestionViewModel.cs
@@ -9,10 +9,59 @@ using System.Windows;
 using System.Windows.Threading;
 using Timer = System.Timers.Timer;
 
-
 namespace Rditil.ViewModels
 {
+    public class QuestionViewModel : ObservableObject
+    {
+        private Timer _timer;
+        private TimeSpan _tempsRestant;
+        private int _index;
+        public ObservableCollection<Question> QuestionsTirees { get; set; }
+        public Question QuestionEnCours { get; set; }
+        public ObservableCollection<ReponseChoix> ReponsesEnCours { get; set; }
 
+        public QuestionViewModel()
+        {
+            QuestionsTirees = new ObservableCollection<Question>();
+            ReponsesEnCours = new ObservableCollection<ReponseChoix>();
+            _timer = new Timer(1000);
+            _timer.Elapsed += (sender, e) =>
+            {
+                _tempsRestant -= TimeSpan.FromSeconds(1);
+                if (_tempsRestant <= TimeSpan.Zero)
+                {
+                    _timer.Stop();
+                    MessageBox.Show("Temps écoulé !", "Fin de l'examen", MessageBoxButton.OK, MessageBoxImage.Information);
+                    // Logique pour terminer l'examen
+                }
+            };
+            _timer.Start();
+        }
 
+        private void ChargerQuestion(int index)
+        {
+            if (index < 0 || index >= QuestionsTirees.Count) return;
+            QuestionEnCours = QuestionsTirees[index];
+            ReponsesEnCours.Clear();
+            foreach (var reponse in QuestionEnCours.Reponses)
+            {
+                ReponsesEnCours.Add(new ReponseChoix { Id = reponse.Id, Texte = reponse.Texte });
+            }
+        }
 
+        private void PasserQuestionSuivante()
+        {
+            if (_index < QuestionsTirees.Count - 1)
+            {
+                _index++;
+                ChargerQuestion(_index);
+            }
+            else
+            {
+                _timer.Stop();
+                MessageBox.Show("Examen terminé !", "Fin de l'examen", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Logique pour terminer l'examen
+            }
+        }
+    }
 }
